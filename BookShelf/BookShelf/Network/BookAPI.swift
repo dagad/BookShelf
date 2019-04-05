7c81f9d3904d22f1b74e0f103726c5867a030cff
dagad <dagad@estsoft.com>
2019-04-05 15:58:23 +0900

    create BookAPI

---
+++ BookShelf/BookShelf/Network/BookAPI.swift
@@ -0,0 +1,50 @@
+//
+//  BookAPI.swift
+//  BookShelf
+//
+//  Created by dagad on 05/04/2019.
+//  Copyright © 2019 dagad. All rights reserved.
+//
+
+import Moya
+
+enum BookAPI {
+    case new
+    case search
+    case detail
+}
+
+extension BookAPI: TargetType {
+    var baseURL: URL {
+        return URL(string: "https://api.itbook.store/1.0")!
+    }
+
+    var path: String {
+        switch self {
+        case .new:
+            return "/new"
+        case .search:
+            return "/search"
+        case .detail:
+            return "books"
+        }
+    }
+
+    var method: Method {
+        return .get
+    }
+
+    var sampleData: Data {
+        return Data()
+    }
+
+    var task: Task {
+        return .requestPlain
+    }
+
+    var headers: [String : String]? {
+        return nil
+    }
+
+
+}
