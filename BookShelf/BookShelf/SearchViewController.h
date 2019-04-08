a30c251678ff989aa25c1c7edf13cce7b07657ac
dagad <devdagad@gmail.com>
2019-04-08 14:07:06 +0900

    Implement SearchVC
    - UICollectionViewDatasource, UICollectionViewDelegate same as NewVC

--- BookShelf/BookShelf/SearchViewController.h
+++ BookShelf/BookShelf/SearchViewController.h
@@ -10,7 +10,13 @@
 
 NS_ASSUME_NONNULL_BEGIN
 
-@interface SearchViewController : UIViewController
+@interface SearchViewController : UIViewController<UISearchBarDelegate, UICollectionViewDataSource, UICollectionViewDelegate>
+
+@property (weak, nonatomic) IBOutlet UISearchBar *searchBar;
+@property (weak, nonatomic) IBOutlet UICollectionView *collectionView;
+@property (weak, nonatomic) IBOutlet UIButton *searchButton;
+
+@property (strong, nonatomic) NSArray *books;
 
 @end
 
--- BookShelf/BookShelf/SearchViewController.m
+++ BookShelf/BookShelf/SearchViewController.m
@@ -7,6 +7,8 @@
 //
 
 #import "SearchViewController.h"
+#import "BookDetailViewController.h"
+#import "BookCollectionViewCell.h"
 #import "BookShelf-Swift.h"
 
 @interface SearchViewController ()
@@ -15,14 +17,68 @@
 
 @implementation SearchViewController
 
+NSInteger currentPage = 0;
+
 - (void)viewDidLoad {
     [super viewDidLoad];
 
     if (@available(iOS 11.0, *)) {
         self.navigationController.navigationBar.prefersLargeTitles = YES;
     }
-    
-    
+
+    self.collectionView.dataSource = self;
+    self.collectionView.delegate = self;
+    [self.collectionView registerNib:[UINib nibWithNibName:@"BookCollectionViewCell" bundle:nil] forCellWithReuseIdentifier:@"bookcell"];
+
+    self.searchBar.delegate = self;
 }
+
+- (void)searchKeyword:(NSString *)keyword page:(NSString *)page {
+    __weak __typeof(self) weakSelf = self;
+    [BookService.shared searchBooksByKeyword:keyword page:page success:^(NSArray<Book *> *books) {
+        weakSelf.books = books;
+        [weakSelf.collectionView reloadData];
+    } failure:^(NSError *error) {
+        // Error Handling
+    }];
+}
+- (IBAction)search:(id)sender {
+    NSString *keyword = self.searchBar.text;
+    [self searchKeyword:keyword page:[@(currentPage) stringValue]];
+}
+
+
+// MARK: - UISearchBarDelegate
+- (void)searchBarTextDidEndEditing:(UISearchBar *)searchBar {
+
+}
+
+// MARK: - UICollectionViewDataSource
+- (UICollectionViewCell *)collectionView:(nonnull UICollectionView *)collectionView cellForItemAtIndexPath:(nonnull NSIndexPath *)indexPath {
+    BookCollectionViewCell *cell = [collectionView dequeueReusableCellWithReuseIdentifier:@"bookcell" forIndexPath:indexPath];
+    [cell configureCellWithBook:[self.books objectAtIndex:indexPath.item]];
+    return cell;
+}
+
+- (NSInteger)collectionView:(nonnull UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section {
+    return [self.books count];
+}
+
+// MARK: - UICollectionViewDelegate
+- (void)collectionView:(UICollectionView *)collectionView didSelectItemAtIndexPath:(NSIndexPath *)indexPath {
+    Book *touchedBook = [self.books objectAtIndex:indexPath.item];
+    BookDetailViewController *bookDetailVC = [[BookDetailViewController alloc] init];
+    [bookDetailVC setBook:touchedBook];
+    [self.navigationController pushViewController:bookDetailVC animated:YES];
+}
+
+// MARK: - UICollectionViewFlowLayoutDelegate
+- (CGSize)collectionView:(UICollectionView *)collectionView layout:(UICollectionViewLayout*)collectionViewLayout sizeForItemAtIndexPath:(NSIndexPath *)indexPath {
+    CGRect screenSize = UIScreen.mainScreen.bounds;
+    CGSize cellSize = CGSizeMake(screenSize.size.width, 105);
+    return cellSize;
+}
+
+
 
 @end
