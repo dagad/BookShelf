481725f9a1976a7ad1def497a5f59573b0dafab9
devdagad <devdagad@gmail.com>
2019-04-09 08:22:58 +0900

    fix minor ui issue

--- BookShelf/BookShelf/Cell/BookCollectionViewCell.m
+++ BookShelf/BookShelf/Cell/BookCollectionViewCell.m
@@ -18,6 +18,14 @@
     [self.subTitleLabel setText:book.subtitle];
     [self.priceLabel setText:book.price];
     [self.isbnLabel setText:book.isbn13];
+    [self hideEmptyLabel];
+}
+
+- (void)hideEmptyLabel {
+    [self.titleLabel setHidden:(self.titleLabel.text.length == 0)];
+    [self.subTitleLabel setHidden:(self.subTitleLabel.text.length == 0)];
+    [self.priceLabel setHidden:(self.priceLabel.text.length == 0)];
+    [self.isbnLabel setHidden:(self.isbnLabel.text.length == 0)];
 }
 
 @end
